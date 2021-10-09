7c15897f653d53908f793b6fbd2cb1e8a6921605
luxuwei <92145301+luxuwei@users.noreply.github.com>
2021-10-09 08:54:59 +0800

    another test

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix change");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test another day");
     }
 }
