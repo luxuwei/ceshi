e9c938cec193cb907cc2cb9a475ec9177f35f392
lxw <330705429@qq.com>
2021-10-09 09:04:22 +0800

    my commit 6

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("pull test another day");
+        System.out.println("git test6");
     }
 }
